7b4e72ca0abea5c89d0f5f70516325fc3d59be1c
Anna <radiant2958@gmail.com>
2024-08-19 17:01:01 +0500

    Add models, serializers, view,tests TeamMember

--- about_us_app/models.py
+++ about_us_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class TeamMember(models.Model):
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name="Фотография")
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    role = models.CharField(max_length=100, verbose_name="Роль в проекте")
+
+    class Meta:
+        verbose_name = "Член команды"
+        verbose_name_plural = "Члены команды"
+
+    def __str__(self):
+        return f"{self.name} {self.last_name} - {self.role}"
--- about_us_app/tests.py
+++ about_us_app/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+from .models import TeamMember
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_create_team_member():
+    team_member = TeamMember.objects.create(
+        name="Иван",
+        last_name="Иванов",
+        role="Разработчик",
+       
+    )
+    assert team_member.name == "Иван"
+    assert team_member.last_name == "Иванов"
+    assert team_member.role == "Разработчик"
+
+@pytest.mark.django_db
+def test_team_member_str():
+    team_member = TeamMember.objects.create(
+        name="Иван",
+        last_name="Иванов",
+        role="Разработчик",
+       
+    )
+    assert str(team_member) == "Иван Иванов - Разработчик"
+
+@pytest.mark.django_db
+def test_team_member_list_api():
+    client = APIClient()
+    response = client.get(reverse('teammember-list'))
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_create_team_member_api():
+    client = APIClient()
+    data = {
+        "name": "Иван",
+        "last_name": "Иванов",
+        "role": "Разработчик",
+       
+    }
+    response = client.post(reverse('teammember-list'), data)
+    assert response.status_code == 201
+    assert response.data['name'] == "Иван"
+
