fd1e627ba0c5e2311d808bb1a94b60ad1f56c4b8
Aydar Bariev <root@HOME-PC>
2024-08-21 20:25:58 +0300

    добавлены описания

--- about_us_app/views.py
+++ about_us_app/views.py
@@ -3,5 +3,6 @@ from .models import TeamMember
 from .serializers import TeamMemberSerializer
 
 class TeamMemberViewSet(viewsets.ModelViewSet):
+    """ API view for team members """
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
--- events_app/views.py
+++ events_app/views.py
@@ -4,6 +4,7 @@ from .serializers import EventsSerializer
 from events_app.paginators import EventsPaginator
 
 class EventsAPIView(viewsets.ModelViewSet):
+    """ API view for events """
     queryset = Events.objects.all()
     serializer_class = EventsSerializer
     pagination_class = EventsPaginator
