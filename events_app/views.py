96c380c5b2e9599b52e58b248a3d9bbb0c3d3f6c
andre059 <andreyperm159@mail.ru>
2024-09-10 17:41:20 +0500

    PEP8

--- events_app/views.py
+++ events_app/views.py
@@ -3,7 +3,6 @@ from .models import Event
 from .serializers import EventsSerializer
 
 
-
 class EventsAPIView(viewsets.ModelViewSet):
     """ API view for events """
     queryset = Event.objects.all()
