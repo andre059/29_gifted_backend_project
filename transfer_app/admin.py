94f9681b87d0f34345d3ed4116a3af847113e457
andre059 <andreyperm159@mail.ru>
2024-09-22 17:09:42 +0500

    rename

--- transfer_app/admin.py
+++ transfer_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from transfer_app.models import Transfer
+from transfer_app.models import Payment
 
 
-@admin.register(Transfer)
-class TransferAdmin(admin.ModelAdmin):
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
     list_display = ('sender', 'recipient', 'amount', 'currency', 'status')
     list_filter = ('status',)
--- transfer_app/urls.py
+++ transfer_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from transfer_app.apps import TransferAppConfig
-from transfer_app.views import TransferViewSet
+from transfer_app.apps import PaymentAppConfig
+from transfer_app.views import PaymentViewSet
 
 
-app_name = TransferAppConfig.name
+app_name = PaymentAppConfig.name
 router = DefaultRouter()
-router.register(r'transfers', TransferViewSet, basename='transfer')
+router.register(r'payment', PaymentViewSet, basename='payment')
 
 urlpatterns = [
     path('', include(router.urls)),
