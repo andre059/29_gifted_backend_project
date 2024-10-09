766725a9f2b306a376600294c59ce29b24214113
rimtimti <wotrimtimti@gmail.com>
2024-10-09 21:59:44 +0300

    Написано ограничение для загрузки файла пользователем в приложениях friends_app и feedback_app

--- friends_app/serializers.py
+++ friends_app/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
 from .models import Friend, Company, Volunteer
+from django.template.defaultfilters import filesizeformat
+
+# Разрешаем загрузку файла со стороны пользователя не более 5 Мб
+MAX_UPLOAD_SIZE = 5 * 1024 * 1024
+
+
+def check_file(self):
+    '''
+    Ограничение размера загружаемого файла
+    '''
+    if self.size > MAX_UPLOAD_SIZE:
+        raise serializers.ValidationError(
+            f"Пожалуйста, не превышайте размер файла {filesizeformat(MAX_UPLOAD_SIZE)}. Текущий размер файла {filesizeformat(self.size)}"
+        )
+    return self
 
 
 class FriendSerializer(serializers.ModelSerializer):
@@ -15,6 +30,8 @@ class CompanySerializer(serializers.ModelSerializer):
 
 
 class VolunteerSerializer(serializers.ModelSerializer):
+    link = serializers.FileField(validators=[check_file])
+
     class Meta:
         model = Volunteer
         fields = "__all__"
