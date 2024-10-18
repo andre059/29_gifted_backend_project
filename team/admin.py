dd3c78e11b7fd36320e94bf504bde52710f684c6
Stas Latyshonok <stasura@gmail.com>
2024-10-18 21:06:59 +0300

    updated team pep8

--- team/admin.py
+++ team/admin.py
@@ -1,5 +1,23 @@
 # team/admin.py
 from django.contrib import admin
-from .models import Developer 
- 
-admin.site.register(Developer)  
+from .models import Developer, DeveloperImage
+
+
+
+class DeveloperImageInline(admin.TabularInline):
+    model = DeveloperImage
+    extra = 1
+
+
+@admin.register(Developer)
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "surname",
+        "role",
+    )
+    search_fields = ("role",)
+    inlines = [
+        DeveloperImageInline,
+    ]
