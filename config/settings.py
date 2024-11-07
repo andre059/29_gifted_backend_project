ea7a9012f83ff946f23b3a771dd5049178893c21
Aydar Bariev <a_bariev@internet.ru>
2024-11-07 18:52:31 +0300

    Настройка статики

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static", 
     ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 MEDIA_URL = "/media/"
