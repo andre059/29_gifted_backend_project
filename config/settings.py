90d25df913bcabc572d188809163eec75338a8e1
Aydar Bariev <a_bariev@internet.ru>
2024-11-07 19:02:46 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -120,9 +120,9 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-    ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static", 
+#     ]
 STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
