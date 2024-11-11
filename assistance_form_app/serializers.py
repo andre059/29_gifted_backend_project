4ce7fb8c6fec184beea532265ba9d132fc35e5e9
Aydar Bariev <a_bariev@internet.ru>
2024-11-11 23:40:12 +0300

    fix_phone for assistance_form

--- assistance_form_app/serializers.py
+++ assistance_form_app/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from assistance_form_app.models import AssistanceForm
+from config.services import fix_phone
 
 class AssistanceFormSerializer(serializers.ModelSerializer):
+    
+    def validate_phone(self, value):
+        return fix_phone(value)
 
     class Meta:
         model = AssistanceForm
