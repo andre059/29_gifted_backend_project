bb3df5191c7e856b9dae9a4ee4de1d01a720c145
Aydar Bariev <a_bariev@internet.ru>
2024-11-12 14:07:59 +0300

    проверка протокола

--- projects_app/serializers.py
+++ projects_app/serializers.py
@@ -3,11 +3,11 @@ from projects_app.models import Project, ProjectImage
 
 
 class ProjectImageSerializer(serializers.ModelSerializer):
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        if instance.link and isinstance(representation.get('link'), str):
-            representation['link'] = representation['link'].replace('http://', 'https://')
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     if instance.link and isinstance(representation.get('link'), str):
+    #         representation['link'] = representation['link'].replace('http://', 'https://')
+    #     return representation
     
     class Meta:
         model = ProjectImage
